c9c361e3d70804e96b945260415413263eff0ba8
alena <alena.vozmishcheva@gmail.com>
2025-03-25 00:09:44 +0300

    Выполнено задание.

---
+++ _02_simple_queries/simple_queries.py
@@ -0,0 +1,88 @@
+import os
+
+from dotenv import load_dotenv
+import pyodbc
+
+import SQL_Queries
+
+load_dotenv()
+
+DRIVER = os.getenv('MS_SQL_DRIVER')
+SERVER = os.getenv('MS_SQL_SERVER')
+WORK_DATABASE = os.getenv('MS_SQL_DATABASE')
+PAD_DATABASE = os.getenv('MS_PAD_DATABASE')
+USER = os.getenv('MS_SQL_USER')
+PASSWORD = os.getenv('MS_SQL_KEY')
+
+# connection_string = f"""DRIVER={{SQL Server}};
+#                         SERVER={SERVER};
+#                         DATABASE={PAD_DATABASE};
+#                         Trusted_Connection=yes"""
+
+connection_string = f"""DRIVER={DRIVER};
+                        SERVER={SERVER};
+                        DATABASE={PAD_DATABASE};
+                        UID={USER};
+                        PWD={PASSWORD};
+                        Trusted_Connection=yes"""
+
+conn = pyodbc.connect(connection_string)
+conn.autocommit = True
+
+try:
+    SQL_QUERY = SQL_Queries.create_database_default(WORK_DATABASE)
+    conn.execute(SQL_QUERY)
+except pyodbc.ProgrammingError as ex:
+    print(ex)
+else:
+    print(f"Database {WORK_DATABASE} Created")
+# finally:
+#     conn.close()
+
+cursor = conn.cursor()
+table_name = "Products"
+
+try:
+    SQL_QUERY = SQL_Queries.create_table(table_name)
+    cursor.execute(f"USE {WORK_DATABASE};")
+    cursor.execute(SQL_QUERY)
+except pyodbc.ProgrammingError as ex:
+    print(ex)
+else:
+    print(f"Table {table_name} Created")
+# finally:
+#     conn.close()
+
+try:
+    SQL_QUERY = SQL_Queries.insert_data_products(table_name)
+    cursor.execute(f"USE {WORK_DATABASE};")
+    cursor.execute(SQL_QUERY)
+except pyodbc.ProgrammingError as PEex:
+    print(PEex)
+except pyodbc.IntegrityError as IEex:
+    print(IEex)
+else:
+    print(f"Data in {table_name} Inserted")
+finally:
+    conn.close()
+
+data_list = []
+try:
+    SQL_QUERY = SQL_Queries.get_data(table_name)
+    cursor.execute(f"USE {WORK_DATABASE};")
+    result = cursor.execute(SQL_QUERY)
+except pyodbc.Error as ex:
+    print(ex)
+else:
+    records = result.fetchall()
+    print(records)
+    for record in records:
+        data_dict = {'id': record.ProductID, 'name': record.ProductName, 'price': float(record.Price)}
+        data_list.append(data_dict)
+finally:
+    conn.close()
+
+print(data_list)
+[print(data) for data in data_list]
+
+
